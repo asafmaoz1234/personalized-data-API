cfb44e4740e36fc62e79bbee702a9336c9237904
asafmaoz1234 <asafmaoz27@gmail.com>
2023-05-08 01:44:43 +0300

    added get shoppers by product with relevancy score

--- src/main/java/com/example/civalue/repositories/ShopperProductRepository.java
+++ src/main/java/com/example/civalue/repositories/ShopperProductRepository.java
@@ -2,8 +2,15 @@ package com.example.civalue.repositories;
 
 import com.example.civalue.daos.ShopperProductsDAO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ShopperProductRepository extends JpaRepository<ShopperProductsDAO, Long> {
+
+    @Query(value = "SELECT * FROM shopper_product " +
+            "WHERE shopper_product.product_id = :productId LIMIT :limit", nativeQuery = true)
+    List<ShopperProductsDAO> getShoppersByProductId(String productId, int limit);
 }
--- src/main/java/com/example/civalue/services/Shopper.java
+++ src/main/java/com/example/civalue/services/Shopper.java
@@ -1,29 +1,40 @@
 package com.example.civalue.services;
 
 import com.example.civalue.client.InternalDataClient;
+import com.example.civalue.daos.ShopperProductsDAO;
 import com.example.civalue.daos.helpers.ShopperProductsDAOHelper;
 import com.example.civalue.repositories.ShopperProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class Shopper {
     private String shopperId;
-    InternalDataClient internalDataClient;
-    ShopperProductRepository shopperProductRepository;
+    private int age;
+    private Double relevancyScore;
 
-    public Shopper(InternalDataClient internalDataClient, ShopperProductRepository shopperProductRepository) {
-        this.internalDataClient = internalDataClient;
-        this.shopperProductRepository = shopperProductRepository;
-    }
+    @Autowired
+    InternalDataClient internalDataClient;
+    @Autowired
+    ShopperProductRepository shopperProductRepository;
 
     public Shopper() {
     }
 
+    public Shopper(String shopperId, Double relevancyScore) {
+        this.shopperId = shopperId;
+        this.relevancyScore = relevancyScore;
+    }
+
     public List<Shopper> getShoppersByProductId(String productId, int limit) {
-        return Collections.emptyList();
+        List<ShopperProductsDAO> shopperProductsDAOS = this.shopperProductRepository.getShoppersByProductId(productId, limit);
+        List<Shopper> response = new ArrayList<>();
+        shopperProductsDAOS.forEach(shopperProductsDAO ->
+                response.add(new Shopper(shopperProductsDAO.getShopperId(),shopperProductsDAO.getRelevancyScore())));
+        return response;
     }
 
     public void saveShopperProducts(String shopperId) {
@@ -44,5 +55,17 @@ public class Shopper {
         return "Shopper{" +
                 "shopperId='" + shopperId + '\'' +
                 '}';
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Double getRelevancyScore() {
+        return relevancyScore;
     }
 }
